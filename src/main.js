d0f4ae84f976eb41659bdf75f254b697ed7ab82a
Tarık Sefa <50182711+Gosmacx@users.noreply.github.com>
2022-04-13 01:32:54 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 
-// import axios from 'redaxios';
-// const appAxios = axios.create({
-//    baseURL: "localhost",
-// })
-// app.config.globalProperties.$appAxios = appAxios;
-
-
 const app =  createApp(App)
-
 
 app.mount('#app')
